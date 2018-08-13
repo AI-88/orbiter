eaa80461e251da30e7f41beddb5c9c80508389d3
isaac1104 <isaac-kwon@hotmail.com>
2018-08-12 20:57:31 -0700

    refactor reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,21 +1,12 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import allUsersDataReducer from './all_users_data_reducer';
-import addNewUsersReducer from './add_new_users_reducer';
 import userAuthReducer from './user_auth_reducer';
 
-const appReducer = combineReducers({
+const rootReducer = combineReducers({
   form: formReducer,
   users: allUsersDataReducer,
-  newUsers: addNewUsersReducer,
   userAuth: userAuthReducer
 });
-
-const rootReducer = (state, action) => {
-  if (action.type === 'RESET_NEW_USERS_REGISTRATION') {
-    state.newUsers = undefined;
-  }
-  return appReducer(state, action);
-};
 
 export default rootReducer;
