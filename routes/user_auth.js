832d7b84b7b1aa3336271c83d3bc371c1ffdeb94
isaac1104 <isaac-kwon@hotmail.com>
2018-08-13 18:16:24 -0700

    route for login added

--- routes/user_auth.js
+++ routes/user_auth.js
@@ -8,4 +8,11 @@ module.exports = app => {
     const { data } = request;
     res.send(data);
   });
+
+  app.post('/api/login', async (req, res) => {
+    const loginUrl = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${keys.webApiKey}`;
+    const request = await axios.post(loginUrl, req.body);
+    const { data } = request;
+    res.send(data);
+  });
 };
