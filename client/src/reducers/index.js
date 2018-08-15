a4edc027a40e8eab7858a073f69e3ddcf3e14700
isaac1104 <isaac-kwon@hotmail.com>
2018-08-14 19:10:34 -0700

    user login reducer added

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import allUsersDataReducer from './all_users_data_reducer';
 import userSignupReducer from './user_signup_reducer';
+import userLoginReducer from './user_login_reducer';
 
 const rootReducer = combineReducers({
   form: formReducer,
   users: allUsersDataReducer,
-  userSignup: userSignupReducer
+  userSignup: userSignupReducer,
+  userLogin: userLoginReducer
 });
 
 export default rootReducer;
