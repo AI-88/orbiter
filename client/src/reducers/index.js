23f07e36afe79007c7d6282756b43a248707b4c8
isaac1104 <isaac-kwon@hotmail.com>
2018-08-15 17:16:03 -0700

    reducer refactor

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,17 +3,10 @@ import { reducer as formReducer } from 'redux-form';
 import allUsersDataReducer from './all_users_data_reducer';
 import userAuthReducer from './user_auth_reducer';
 
-const appReducer = combineReducers({
+const rootReducer = combineReducers({
   form: formReducer,
   users: allUsersDataReducer,
   userAuth: userAuthReducer
 });
-
-const rootReducer = (state, action) => {
-  if (action.type === 'USER_AUTH_RESET') {
-    state.userAuth = undefined;
-  }
-  return appReducer(state, action);
-};
 
 export default rootReducer;
