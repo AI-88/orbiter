27025b373e69db05767d1ef2a9991e84a185d943
isaac1104 <isaac-kwon@hotmail.com>
2018-08-20 17:23:43 -0700

    center signup & login forms

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,9 +2,18 @@ import React from 'react';
 import LoginForm from './Form/LoginForm';
 
 const Login = () => {
+  const style = {
+    container: {
+      height: '100vh',
+      overflow: 'hidden',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  };
+
   return (
-    <div>
-      <h1>Login Route</h1>
+    <div style={style.container}>
       <LoginForm />
     </div>
   );
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -2,9 +2,18 @@ import React from 'react';
 import SignupForm from './Form/SignupForm';
 
 const Signup = () => {
+  const style = {
+    container: {
+      height: '100vh',
+      overflow: 'hidden',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  };
+
   return (
-    <div>
-      <h1>Signup Route</h1>
+    <div style={style.container}>
       <SignupForm />
     </div>
   );
